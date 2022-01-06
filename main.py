87663f34c194a92483b9dfb4c9cbea0742ecccdf
Felipe Santos <lipe.fssantos063@gmail.com>
2022-01-06 14:00:54 -0300

    algoritmo ja ta rodando

--- main.py
+++ main.py
@@ -5,9 +5,9 @@ import numpy as np
 import tkinter as TK
 
 s = turtle.getscreen()  
-turtle.title("Testandoooo")
+turtle.title("Picasso")
 t = turtle.Turtle()
-
+t.width(2) 
 #SETUP
 
 LENGTH = 260 #ajusta de acordo com a imagem
@@ -67,18 +67,38 @@ def set_goto_pen(dot_i, dot_n, based=0):
     t.goto(list_axisX[dot_n], list_axisY[dot_n])
 
 ## Begin-OpenCV & Draw on Turtle Window 
-#CONTA OS PIXELS
-cimg = img.copy()
+#CONTA OS PIXELS 
+node_root = 0
+node_crrt = 0
 
-before_cnt = np.sum(cimg == 0)
-cv2.line(cimg, (int(list_axisXIMG[30]), int(list_axisYIMG[30])), (int(list_axisXIMG[143]), int(list_axisYIMG[143])), (255, 255, 255), thickness=2)
-after_cnt = np.sum(cimg == 0)
+#while np.sum(img == 0) != 0:
 
-in_that_line = before_cnt - after_cnt
+while True:
+    max_v = -(10**9)
+    for i in range(len(list_axisXIMG)):
+        cimg = img.copy()
 
-print(in_that_line)
+        before_cnt = np.sum(cimg == 0)
+        cv2.line(cimg, (int(list_axisXIMG[node_root]), int(list_axisYIMG[node_root])),
+        (int(list_axisXIMG[i]), int(list_axisYIMG[i])), (255, 255, 255), thickness=2)
+        after_cnt = np.sum(cimg == 0)
+
+        in_that_line = before_cnt - after_cnt
+        if in_that_line > max_v:
+            max_v = in_that_line
+            node_crrt = i
+
+    cv2.line(img, (int(list_axisXIMG[node_root]), int(list_axisYIMG[node_root])),
+        (int(list_axisXIMG[node_crrt]), int(list_axisYIMG[node_crrt])), (255, 255, 255), thickness=2)
+    #draw in turtle
+    set_goto_pen(node_root, node_crrt)
+
+    node_root = node_crrt
+    if max_v == 0:
+        break
+    print(node_crrt)
+    print(max_v)
 #FIM DA CONTAGEM
-
 
 '''
 print(len(list_axisY))
